dc07c0bf05bfd1210f454c531a648aa2ed5d61de
DAKSH SAHU <dakshsahu24@gmail.com>
2024-05-10 10:23:27 +0530

    Revert "insurance component"
    This reverts commit 34d60fb10c1b1f5fde08f49ea465b00e7c4dac99.

--- client/src/informationcard/InformationCard.js
+++ client/src/informationcard/InformationCard.js
@@ -1,36 +1,15 @@
 import React from "react";
-import "./informationcard.css";
 
-function InformationCard({ title, price, description }) {
+function InformationCard(props) {
   return (
-    <div className="information-container">
-      <div className="box">
-        <div>
-          <h5>{title}</h5>
-          <h2>
-            ${price}
-            <span>/month</span>
-          </h2>
-        </div>
-        <div>
-          <ul>
-            <li className="tick">Item 1</li>
-            <li className="tick">Item 2</li>
-            <li className="tick">Item 3</li>
-          </ul>
-        </div>
-        <div>
-        <button className="information-button">Learn more</button>
-        <div>Reach out for a customer plan<br></br>
-        <a>here</a></div>
-        </div>
-      </div>
+    <div className="info-cards">
+      <span className="info-card-icon">
+        {/* <FontAwesomeIcon className="info-fa-icon" icon={props.icon} /> */}
+      </span>
+      <p className="info-card-title">{props.title}</p>
+      <p className="info-card-description">{props.description}</p>
     </div>
   );
 }
-
+   
 export default InformationCard;
-
-// 4 Homepages
-// Full UI Kit
-// Other important point
